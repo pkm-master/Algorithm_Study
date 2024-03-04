26cb784e3021e945477821786d1da333e6c5b13e
장유진 <danya6@naver.com>
2024-03-05 07:10:18 +0900

    edit

--- Dingurur/week4/problem_2578_fail.py
+++ Dingurur/week4/problem_2578_fail.py
@@ -4,21 +4,32 @@ for _ in range(5):
     calling += list(input().split())
 
 cnt=0
-scnt=0
-while cnt<=3:
-    for idx, elem in enumerate(calling) :
-        for i in range(5):
-            for j in range(5):
+bingo = False
+def findbingo(board):
+    global cnt, bingo
+    tcnt = 0
+    scnt = 0
+    ascnt = 0
+    for i in range(5):
+        for j in range(5):
+            if cnt == 3:
+                bingo = True
+                break
+            if all(type(elem) != str for elem in board[i]):
+                cnt += 1
+            if board[i][i]==0:
+                scnt+=1
+                if scnt ==5:
+                    cnt+=1
+            if board[i][4-i]==0:
+                ascnt+=1
+                if ascnt ==5:
+                    cnt+=1
+for idx, elem in enumerate(calling) :
+    for i in range(5):
+        for j in range(5):
                 if board[i][j]==elem:
                     board[i][j]=1
-                if board[i].count(1)==5:
-                    cnt += 1
-                if board[j].count(1)==5:
-                    cnt += 1
-    #         # if board[i][i]==0:
-    #         #     scnt+=1
-    #         #     if scnt ==5:
-    #         #         cnt+=1
-
-    print(board)
-    print(cnt)
+                    findbingo(board)
+                    if bingo:
+                        print(idx)
