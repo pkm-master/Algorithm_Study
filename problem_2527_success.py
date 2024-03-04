ee7ceae7ae90c1a884f86c2430b5fe8f1281a34d
gimhaen <snnsprng043@gmail.com>
2024-03-05 00:11:17 +0900

    1

---
+++ problem_2527_success.py
@@ -0,0 +1,16 @@
+for _ in range(4):
+    x1, y1, p1, q1, x2, y2, p2, q2 = map(int, input().split())
+    result = ''
+    if p1<x2 or p2<x1 or q1<y2 or q2<y1:
+        result = 'd'
+    elif p1==x2 or p2==x1:
+        if q1==y2 or q2==y1:
+            result = 'c'
+        else:
+            result ='b'
+    elif q1==y2 or q2==y1:
+        result = 'b'
+    else:
+        result = 'a'
+    print(result)
+
